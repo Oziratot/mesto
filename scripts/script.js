8549317d121e9b837391e37afa705ef05b1e9670
Roman Zavalii <romasty_92@mail.ru>
2020-07-08 23:18:55 +0300

    second iteration

--- scripts/script.js
+++ scripts/script.js
@@ -3,16 +3,16 @@ let popup = document.querySelector('.popup');
 let formElement = popup.querySelector('.popup__container');
 let editButton = document.querySelector('.profile__edit-btn');
 let closeButton = page.querySelector('.popup__close-btn');
-console.log('form found')
+let inputName = document.querySelector('.popup__input_name');
+let inputDescription = document.querySelector('.popup__input_description');
+let name = document.querySelector('.profile__name');
+let description = document.querySelector('.profile__description');
+
 
 function popupOpen() {
-  let inputName = document.querySelector('.popup__input_name');
-  let currentName = document.querySelector('.profile__name').textContent;
-  let inputDescription = document.querySelector('.popup__input_description');
-  let currentDescription = document.querySelector('.profile__description').textContent;
   popup.classList.add('popup_opened');
-  inputName.setAttribute('value', currentName);
-  inputDescription.setAttribute('value', currentDescription);
+  inputName.value = name.textContent;
+  inputDescription.value = description.textContent;
 }
 
 function popupClose() {
@@ -21,12 +21,8 @@ function popupClose() {
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
-  let inputName = page.querySelector('.popup__input_name').value;
-  let inputDescription = page.querySelector('.popup__input_description').value;
-  let newName = page.querySelector('.profile__name');
-  let newDescription = page.querySelector('.profile__description');
-  newName.textContent = inputName;
-  newDescription.textContent = inputDescription;
+  name.textContent = inputName.value;
+  description.textContent = inputDescription.value;
   popupClose();
 }
 
